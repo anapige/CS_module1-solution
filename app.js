086c6ce701335dcfd8eca7cb7464e41cd2fe9134
anapige <140107625+anapige@users.noreply.github.com>
2023-07-20 15:04:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-var LunchCheck = angular.module('LunchCheck', []);
+(function () {
+  'use strict';
+  angular
+    .module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
 
+  LunchCheckController.$inject = ['$scope'];
 
-LunchCheck.controller('LunchCheckController', LunchCheckController)
+  function LunchCheckController($scope) {
+    $scope.name = '';
+    $scope.nbr = 0;
+    
+    $scope.countItem = function () {
+      if ($scope.name == '') {
+      var  nbr = [];
+      } else {
+      var  nbr = $scope.name.split(',').filter((s) => s);
+      }
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope){
+      $scope.nbr = nbr.length;
+      if ($scope.nbr > 0) {
+        $scope.msg = setMsg($scope.nbr);
+        $scope.stil = 'ok';
+        $scope.stil2 = 'ok2';
+      } else {
+        $scope.msg = 'Please enter data first';
+        $scope.stil = 'nok';
+        $scope.stil2 = 'nok2';
+      }
+    };
 
-$scope.name = '' ;
-$scope.nbr = 0 ; 
-
-$scope.countItem = function(){
-if($scope.name ==''){
-  nbr=[];
-}else{
-nbr = $scope.name.split(",").filter(s => s);
-}
-
-$scope.nbr = nbr.length ;
-if($scope.nbr>0){
-$scope.msg = setMsg($scope.nbr);
-$scope.stil = 'ok';
-$scope.stil2 = 'ok2';
-}
-else{
-$scope.msg='Please enter data first';
-$scope.stil = 'nok';
-$scope.stil2 = 'nok2';
-}
-} ;
-
-function setMsg(nbr){
-  if(nbr <= 3)
-  return 'Enjoy';
-  else
-  return 'Too much!'
-}
-}
+    function setMsg(nbr) {
+      if (nbr <= 3) return 'Enjoy';
+      else return 'Too much!';
+    }
+  }
+})();
